147fba8be4369d8b43ca3628faf428af2e9b2ff9
Artur Ibragimov <ilyachever@gmail.com>
2022-10-12 13:59:25 +0400

    Исправил функции

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function getRandomInteger (min, max) {
   const lower = Math.ceil(Math.min(min, max));
   const upper = Math.floor(Math.max(min, max));
-  const result = Math.round(Math.random() * (upper - lower + 1) + lower);
+  const result = Math.floor(Math.random() * (upper - lower + 1) + lower);
 
   if (min < 0 || max < 0 || max <= min) {
     return NaN;
@@ -11,10 +11,5 @@ function getRandomInteger (min, max) {
 }
 
 function checkMaxLength (currentString, maxLength = 140) {
-  if (!(currentString.length <= maxLength)){
-    return NaN;
-  }
-
-  return currentString;
+  return currentString.length <= length;
 }
-
